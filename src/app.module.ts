df435396efd019736924ae0727badf1a3e9d50af
dariavasilega <dariavasilega@gmail.com>
2021-11-30 23:50:44 +0200

    added user table

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            url: process.env.DATABASE,
+            entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+            ssl: process.env.NODE_ENV === 'production',
+            synchronize: true,
+        }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,22 @@
+import { IsEmail, IsEnum, IsNotEmpty } from 'class-validator';
+import { Role } from 'src/core/enums/roles.enum';
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    firstname: string;
+
+    @IsNotEmpty()
+    lastname: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    password: string;
+
+    @IsNotEmpty()
+    photo: string;
+
+    @IsEnum(Role)
+    role: 'student' | 'teacher' | 'administrator';
+}
