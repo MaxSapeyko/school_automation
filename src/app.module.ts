bedc93312a8d1883822766ce2c5284d9151f1b1d
dariavasilega <dariavasilega@gmail.com>
2021-12-04 18:58:50 +0200

    added news table

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NewsModule } from './news/news.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -20,6 +22,8 @@ import { AppService } from './app.service';
       ssl: process.env.NODE_ENV === 'production',
       synchronize: true,
     }),
+    UserModule,
+    NewsModule
   ],
   controllers: [AppController],
   providers: [AppService],
