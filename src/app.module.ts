84a3501f8e293f12501e5c1e6bbf51b599994410
dariavasilega <dariavasilega@gmail.com>
2021-12-04 18:28:47 +0200

    connected DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,15 +9,19 @@ import { AppService } from './app.service';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            url: process.env.DATABASE,
-            entities: [join(__dirname, '**', '*.entity.{ts,js}')],
-            ssl: process.env.NODE_ENV === 'production',
-            synchronize: true,
-        }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      ssl: process.env.NODE_ENV === 'production',
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
